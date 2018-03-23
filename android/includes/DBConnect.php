16bbf56eac2660858d27ea7f79ca78daa0a8d2c0
khayrul-atiq <khayrul.atiq@gmail.com>
2018-03-23 21:29:49 +0600

    hello world test done

--- android/includes/DBConnect.php
+++ android/includes/DBConnect.php
@@ -1,6 +1,22 @@
 <?php  
 
 	
+		class DBConnect
+		{
+			private $conn;
+
+			function __construct()
+			{
+				# code...
+			}
+
+
+
+			function connect() {
+				include_once('constants.php');
+				$this->
+			}
+		}	
 
 
 
