ddabb04049a4bfa87d10269014cf9b77460331dc
khayrul-atiq <khayrul.atiq@gmail.com>
2018-03-24 00:09:11 +0600

    sleeping time

--- android/RegisterUser.php
+++ android/RegisterUser.php
@@ -1,20 +1,37 @@
 <?php  
 
+
+	require_once('User.php');
+	$response = array();
+
 	if($_SERVET['REQUEST_METHOD'] == 'POST') {
-
+		storeUserInformation($response);
 	} else {
-		
-
-
-
+		invalidRequestMethod($response);
 	}
 
+	echo json_encode($response);
 
 
 
+	
 
+	function invalidRequestMethod($response) {
+		$response['error'] = true;
+		$response['message'] = 'invalid request method';
+	}
 
-
+	function storeUserInformation($response) {
+		$user = new User();
+		$user->createUserAccount(
+			$_POST['name'],
+			$_POST['email'],
+			$_POST['password'],
+			'0'
+		);
+		$response['error'] = false;
+		$response['message'] = 'user account successfully created';
+	}
 
 
 ?>
